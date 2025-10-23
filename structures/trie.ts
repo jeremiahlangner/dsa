45652df55256898bfb47aea91f2784662974ff3e
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2025-10-23 08:48:26 -0700

    feat: add basic trie code

--- structures/trie.ts
+++ structures/trie.ts
@@ -1,7 +1,48 @@
-type TrieNode = {
-}
+import assert from "node:test";
 
-export class Trie {
-  constructor() {
+type Letter<N extends number> = {
+  0: string,
+  length: N,
+} & string;
+
+type TrieNode = {
+  [l: Letter<1>]: TrieNode | null,
+  terminal?: boolean,
+};
+
+export default class Trie {
+  root: TrieNode = {};
+
+  insert(word: string) {
+    let node = this.root;
+    for (const letter of word) {
+      if (!node[letter]) node[letter] = {};
+      node = node[letter];
+    }
+    node.terminal = true;
+  }
+
+  search(word: string) {
+    let node = this.root;
+    for (const letter of word) {
+      if (!node[letter]) return false;
+      node = node[letter];
+    }
+    return node.terminal;
+  }
+
+  startsWith(word: string) {
+    let node = this.root;
+    for (const letter of word) {
+      if (!node[letter]) return false;
+      node = node[letter];
+    }
+    return true;
   }
 }
+
+const trie = new Trie();
+trie.insert("hello");
+trie.insert("world");
+assert(trie.search("hello") === true);
+assert(trie.search("world") === true);
